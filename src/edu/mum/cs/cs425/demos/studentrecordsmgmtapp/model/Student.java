7825dbbc50837a6fce46aee8236836e3d55a6221
abanoub kaiser <akaiser@miu.edu>
2020-03-04 02:31:18 -0600

    First commit

---
+++ src/edu/mum/cs/cs425/demos/studentrecordsmgmtapp/model/Student.java
@@ -0,0 +1,40 @@
+package edu.mum.cs.cs425.demos.studentrecordsmgmtapp.model;
+import java.time.LocalDate;
+
+public class Student {
+
+    int studentId;
+    String name;
+    LocalDate dateOfAdmission;
+
+    public Student(int studentId, String name, LocalDate dateOfAdmission) {
+        this.studentId = studentId;
+        this.name = name;
+        this.dateOfAdmission = dateOfAdmission;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDateOfAdmission() {
+        return dateOfAdmission;
+    }
+
+    public void setDateOfAdmission(LocalDate dateOfAdmission) {
+        this.dateOfAdmission = dateOfAdmission;
+    }
+}
+
